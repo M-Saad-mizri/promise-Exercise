b7282395f4d239a77ccad6bbb191d6b92604e251
Muhammad Saad <saadmizri434@gmail.com>
2023-10-01 09:30:08 +0500

    Update promiseCode.js
    updated

--- promiseCode.js
+++ promiseCode.js
@@ -1,72 +1,72 @@
-// const button = document.getElementById("btn");
-// const checkerDisplay = document.getElementById("checkerr");
-// var mg = document.querySelector("img");
-
-// function runPromise() {
-//   return new Promise((resolve, reject) => {
-//     mg.src = '';
-//     let condition = Math.floor(Math.random() * 2);
-//     mg.alt = "Image Loading...";
-//     setTimeout(() => {
-//       if (condition === 1) {
-//         resolve(condition);
-//       } else {
-//         reject(condition);
-//       }
-//     }, 2000);
-//   })
-//   .then((condition) => {
-//         // console.log("promise one resolved");
-//         mg.src = "sweet.png";
-//         checkerDisplay.innerText = condition;
-//     })
-//     .catch((condition) => {
-//         checkerDisplay.innerText = condition;
-//             mg.src = "";
-//             mg.alt = "Error in loading the image";
-//             // console.log("promise one rejected");
-//         });
-// }
-
-// button.addEventListener("click", () => {
-//   runPromise();
-// });
-
-
 const button = document.getElementById("btn");
 const checkerDisplay = document.getElementById("checkerr");
 var mg = document.querySelector("img");
-function imageLoaded() {
-    // Math.floor(Math.random() * 1.5)
-  var checker = Math.floor(Math.random() * 1.5);
-  checkerDisplay.innerText = checker;
+
+function runPromise() {
   return new Promise((resolve, reject) => {
+    mg.src = '';
+    let condition = Math.floor(Math.random() * 2);
     mg.alt = "Image Loading...";
     setTimeout(() => {
-      if (checker === 1) {
-        console.log(`its onspot checker: ${checker}`);
-        resolve();
-      } else reject();
+      if (condition === 1) {
+        resolve(condition);
+      } else {
+        reject(condition);
+      }
     }, 2000);
-  });
+  })
+  .then((condition) => {
+        // console.log("promise one resolved");
+        mg.src = "sweet.png";
+        checkerDisplay.innerText = condition;
+    })
+    .catch((condition) => {
+        checkerDisplay.innerText = condition;
+            mg.src = "";
+            mg.alt = "Error in loading the image";
+            // console.log("promise one rejected");
+        });
 }
 
 button.addEventListener("click", () => {
-  imageLoaded().then(() => {
-    console.log("promise one resolved");
-    mg.src = "sweet.png";
+  runPromise();
 });
 
-imageLoaded().catch(() => {
-    mg.src = "";
-    mg.alt = "Error in loading the image";
-    console.log("promise one rejected");
-});
 
-imageLoaded().finally(() => {
-    console.log("Promise 1 ended");
-});
-});
+// const button = document.getElementById("btn");
+// const checkerDisplay = document.getElementById("checkerr");
+// var mg = document.querySelector("img");
+// function imageLoaded() {
+//     // Math.floor(Math.random() * 1.5)
+//   var checker = Math.floor(Math.random() * 1.5);
+//   checkerDisplay.innerText = checker;
+//   return new Promise((resolve, reject) => {
+//     mg.alt = "Image Loading...";
+//     setTimeout(() => {
+//       if (checker === 1) {
+//         console.log(`its onspot checker: ${checker}`);
+//         resolve();
+//       } else reject();
+//     }, 2000);
+//   });
+// }
+
+// button.addEventListener("click", () => {
+//   imageLoaded().then(() => {
+//     console.log("promise one resolved");
+//     mg.src = "sweet.png";
+// });
+
+// imageLoaded().catch(() => {
+//     mg.src = "";
+//     mg.alt = "Error in loading the image";
+//     console.log("promise one rejected");
+// });
+
+// imageLoaded().finally(() => {
+//     console.log("Promise 1 ended");
+// });
+// });
 
 // var userObj = {
 //     Name: 'Muhammad Saad',
